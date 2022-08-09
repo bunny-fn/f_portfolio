bc0450c77075b07605f1cb359b2af113cd8e24ff
bunny <hymnnn28@gmail.com>
2022-08-10 01:04:50 +0900

    test 7

--- js/common.js
+++ js/common.js
@@ -1,19 +1,51 @@
-// intro
-let introTxt1 = document.getElementById('intro-txt1');
-let introTxt2 = document.getElementById('intro-txt2');
+/*
+ *  intro text
+ */ 
+// let introTxt1 = document.getElementById('intro-txt1');
+// let introTxt2 = document.getElementById('intro-txt2');
 
-introTxt1.addEventListener('input', function() {
-  this.setAttribute('data-heading', this.innerText);
-});
+// introTxt1.addEventListener('input', function() {
+//   this.setAttribute('data-heading', this.innerText);
+// });
 
-introTxt2.addEventListener('input', function() {
-    this.setAttribute('data-heading', this.innerText);
-});
-
-
+// introTxt2.addEventListener('input', function() {
+//     this.setAttribute('data-heading', this.innerText);
+// });
 
 
-// aside
+/**
+ * responsive navbar
+ */
+const navToggler = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.navbar ul');
+const navLinks = document.querySelectorAll('.navbar a');
+
+
+allEventListners();
+
+
+function allEventListners() {
+  navToggler.addEventListener('click', togglerClick);
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+
+function togglerClick() {
+  navToggler.classList.toggle('toggle-open');
+  navMenu.classList.toggle('open');
+}
+
+
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
+
+
+/*
+ *  aside
+ */ 
 const topMove = document.getElementById('top-move');
 const intro = document.getElementById('intro');
 const about = document.getElementById('about');
@@ -23,13 +55,12 @@ const headerContainer = document.getElementById('header-container');
 
 const checkScroll = () => {
 
-    // 페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 변환) pageOffset
+    // 페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀) pageOffset
     let pageOffset = window.pageYOffset;
 
-    // 이 값이 0이 아니면, 클래스를 show를 선언하고, 그렇지 않다면 show를 삭제한다. 디폴트 css는 hidden상태
-    // 0이면 스크롤이 안된상태 이고, 0이 아니면 스크롤이 일어난상태 
+    // 0이 아니면 show class add, 그렇지 않으면 show class remove // default css => hidden
+    // 0이면 스크롤이 안 된상태 이고, 0이 아니면 스크롤이 일어난 상태 
     if ( pageYOffset !== 0 ) {
-        // id= backtotop  class="show"를 추가한다.
         topMove.classList.add('show');  
     } else {
         topMove.classList.remove('show'); 
@@ -56,7 +87,9 @@ const checkScroll = () => {
     // }
 
 
-    // header menu class
+    /*
+    *  header menu class
+    */ 
     if ( pageOffset >= 350 ) {
         // menu 지나면 menu all white
         headerContainer.classList.remove('header-container');
@@ -76,20 +109,67 @@ const checkScroll = () => {
 
 const topMoveScroll = () => {
     if ( window.pageYOffset > 0 ) {
-        //스무스하게 스크롤 하기 //어디까지 올라갈지 위치를 정한다. behavior 자연스럽게 이동.
         window.scrollTo({top:0, behavior: 'smooth'});
     }
     
 }
 
 
-// 스크롤할때마다, checkScroll을 호출해라.
+// 스크롤 시 checkScroll 호출
 window.addEventListener('scroll', checkScroll);
-// 클릭하면 oveBackToTop를 호출해라
+// top move 버튼 클릭 시 oveBackToTop 호출
 topMove.addEventListener('click', topMoveScroll);
 
 
 
 
+/*
+*  project modal
+*/ 
+const openButton = document.getElementById('open');
+const modal = document.querySelector('.modal');
+const modal_overlay = document.querySelectorAll("modal_overlay");
 
-//header
+const openModal = (param) => {
+    document.getElementById('modal-' + param).classList.remove('modal-hidden');
+}
+
+const closeModal = (param) => {
+    document.getElementById('modal-' + param).classList.add('modal-hidden');
+}
+
+
+
+
+/**
+ * project img slide
+ */
+ lightbox.option ({
+  'resizeDuration': 0,
+  'fadeDuration':100,
+  'wrapAround': true,
+  'disableScrolling': false,
+  'fitImagesInViewport':true,
+  'albumLabel':false,
+  'showImageNumberLabel':false,
+  'alwaysShowNavOnTouchDevices':true,
+//   'maxWidth':1600,
+//   'maxHeight':1000,
+  'positionFromTop':200
+})
+
+
+
+/**
+ * mobile
+ */
+ var isMobile = (function (a) {
+    return (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    );
+  })(navigator.userAgent || navigator.vendor || window.opera);
