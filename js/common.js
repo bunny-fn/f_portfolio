05fb7f149084215b494401551566e2f13d0a860a
bunny <hymnnn28@gmail.com>
2022-08-10 17:05:37 +0900

    test 12

--- js/common.js
+++ js/common.js
@@ -171,3 +171,12 @@ if ( navigator.platform ) {
 
   }
 }
+
+/**
+ * Internet Explorer check
+ */
+let agent = navigator.userAgent.toLowerCase();
+
+if ( (navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1) ) {
+  alert('Internet Explorer는 호환되지 않는 브라우저 입니다.')
+}
